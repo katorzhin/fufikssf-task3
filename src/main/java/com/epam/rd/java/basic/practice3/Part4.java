2d44498b62e1eb0b78c8586432817c6ee13cab8d
fuFIKSSF <[Your>
2020-12-15 23:27:01 +0200

    added part3 and part4

---
+++ src/main/java/com/epam/rd/java/basic/practice3/Part4.java
@@ -0,0 +1,53 @@
+package com.epam.rd.java.basic.practice3;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Part4 {
+
+    public static void main(String[] args) {
+
+        final String string = "asdf";
+        Part4.hash(string, "MD5");
+        Part4.hash(string, "SHA-256");
+
+    }
+
+    public static String hash(String input, String algorithm) {
+        try {
+            StringBuilder stringBuilder = new StringBuilder();
+            MessageDigest digest = MessageDigest.getInstance(algorithm);
+            digest.update(input.getBytes());
+            byte[] arrayHash = digest.digest();
+
+            for (byte element : arrayHash) {
+                stringBuilder.append(convertToHex(element));
+            }
+
+            return stringBuilder.toString();
+
+        } catch (NoSuchAlgorithmException ex) {
+            return null;
+        }
+    }
+
+    private static String convertToHex(byte someByte) {
+        String string = Integer.toHexString(someByte);
+        if (someByte < 0) {
+            string = string.substring(string.length() - 2);
+        }
+        if (string.length() == 1) {
+            string = "0" + string;
+        }
+        return allLettersToUpperCase(string);
+    }
+
+    private static String allLettersToUpperCase(String string) {
+        char[] array = string.toCharArray();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char element : array) {
+            stringBuilder.append(Character.toUpperCase(element));
+        }
+        return stringBuilder.toString();
+    }
+}
