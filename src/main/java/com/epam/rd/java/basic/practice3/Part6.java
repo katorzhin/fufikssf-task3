854e891637c74ab7d3af7a8ef49882ea794ae1c1
fuFIKSSF <[Your>
2020-12-19 00:40:18 +0200

    added part6

---
+++ src/main/java/com/epam/rd/java/basic/practice3/Part6.java
@@ -0,0 +1,55 @@
+package com.epam.rd.java.basic.practice3;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Part6 {
+
+    private static final String TEXT = Util.readFile("part6.txt");
+    private static final String REGEX = "[.[^\\s\\n]]+";
+    private static final Pattern PATTERN = Pattern.compile(REGEX);
+
+    public static void main(String[] args) {
+
+        System.out.println(Part6.convert(TEXT));
+
+    }
+
+    public static String convert(String input) {
+
+        Matcher matcher1;
+        Matcher matcher2;
+        String[] stringsOfText = input.split("\n");
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (String string : stringsOfText) {
+
+            matcher1 = PATTERN.matcher(string);
+
+            while (matcher1.find()) {
+
+                int count = 0;
+                String tempWord = matcher1.group();
+                matcher2 = PATTERN.matcher(input);
+
+                while (matcher2.find()) {
+
+                    if (matcher2.group().equals(tempWord)) {
+
+                        count++;
+                        if (count >= 2) {
+                            stringBuilder.append("_");
+                            break;
+                        }
+
+                    }
+
+                }
+                stringBuilder.append(tempWord).append(" ");
+            }
+            stringBuilder.replace(stringBuilder.length() - 1, stringBuilder.length(), "\n");
+
+        }
+        return stringBuilder.delete(stringBuilder.length() - 1, stringBuilder.length()).toString();
+    }
+}
