a7e3f34c52729c3b4a7d8804c80fc26443b8f533
fuFIKSSF <[Your>
2020-12-20 23:51:41 +0200

    added util

---
+++ src/main/java/com/epam/rd/java/basic/practice3/Util.java
@@ -0,0 +1,25 @@
+package com.epam.rd.java.basic.practice3;
+
+import java.io.IOException;
+import java.nio.file.*;
+
+public class Util {
+
+    private static final String ENCODING = "cp1251";
+
+    public static String readFile(String path) {
+        String res;
+        try {
+            byte[] bytes = Files.readAllBytes(Paths.get(path));
+            res = new String(bytes, ENCODING);
+        } catch (IOException ex) {
+            return null;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(readFile("part1.txt"));
+    }
+
+}
