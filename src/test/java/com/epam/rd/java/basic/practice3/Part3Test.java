6284aac9928223c361525643eab92dfae9d94fd6
fuFIKSSF <[Your>
2020-12-23 23:40:55 +0200

    added 2 tests to part two and three

---
+++ src/test/java/com/epam/rd/java/basic/practice3/Part3Test.java
@@ -0,0 +1,19 @@
+package com.epam.rd.java.basic.practice3;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Part3Test {
+
+    private static final String TEXT = Util.readFile("part3.txt");
+
+    @Test
+    public void convertTest(){
+
+        final String expectedString =
+                "when I Was Younger\n" +
+                        "I Never Needed";
+        Assert.assertEquals(expectedString, Part3.convert(TEXT));
+
+    }
+}
